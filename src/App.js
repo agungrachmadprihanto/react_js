302a6034987541d014bcbe2b351d3a1dbcff21b5
Ajung007 <agungrachmadprihanto@gmail.com>
2023-12-19 10:46:17 +0700

    hello react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="Parentbox">
+      <FotoProduk></FotoProduk>
+      <Keterangan kategori = "ini sebuah logo"></Keterangan>
+    </div>
+  );
+}
+
+function FotoProduk()
+{
+  return (
+    <div className='Foto'>
+        <img src='aku.png'></img>
+    </div>
+  );
+
+}
+
+function Keterangan(props)
+{
+ const {kategori}  = props;
+  return (
+    <div className='Deskripsi'>
+        <p className='Judul'>{kategori}</p>
+        <h1 className='Title'>Hello World</h1>
+        <p className='Price'>IDR 73.000.000</p>
+        <p className='Info'>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
